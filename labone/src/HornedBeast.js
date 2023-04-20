968e44f5cf20baed8939d99eca40f6eebde30a54
Daron Faulkner Jr <dj200099@gmail.com>
2023-04-20 16:53:19 -0500

    lab 02 completed

--- labone/src/HornedBeast.js
+++ labone/src/HornedBeast.js
@@ -1,14 +1,21 @@
-import React from "react"
-
+import React, { useState } from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card} from 'reactstrap'
 
 function HornedBeast(props) {
+    const [clicks, setClicks] = useState(0);
     return (
-      <div>
+      <Card>
         <h2>{props.title}</h2>   {/*all of these are accessing the properties in the HornedBeast and putting them where they need to be */}
-       <img src={props.imageUrl} alt={props.keyword} title={props.title} width="300px"/>
+       <img onClick = {()=> setClicks(clicks + 1)}  src={props.imageUrl} alt={props.keyword} title={props.title} width="300px"/>
        <p>{props.description}</p>
-      </div>
+       <p> &#9829;{clicks}</p>
+      </Card>
     );
+        
+
+
+
   }
   
   export default HornedBeast;
