e9b744ea7725acd201836e0a99b5e623699a80a5
Daron Faulkner Jr <dj200099@gmail.com>
2023-04-20 17:12:10 -0500

    added comments

--- labone/src/HornedBeast.js
+++ labone/src/HornedBeast.js
@@ -3,13 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card} from 'reactstrap'
 
 function HornedBeast(props) {
-    const [clicks, setClicks] = useState(0);
+    const [clicks, setClicks] = useState(0); // Creating and setting the state for the HornedBeast function.
     return (
       <Card>
         <h2>{props.title}</h2>   {/*all of these are accessing the properties in the HornedBeast and putting them where they need to be */}
-       <img onClick = {()=> setClicks(clicks + 1)}  src={props.imageUrl} alt={props.keyword} title={props.title} width="300px"/>
+       <img onClick = {()=> setClicks(clicks + 1)} src={props.imageUrl} alt={props.keyword} title={props.title} width="300px"/> {/* added the attribute onClick and made the function for them to increase by one every time that the image is clicked on.  */}
        <p>{props.description}</p>
-       <p> &#9829;{clicks}</p>
+       <p> &#9829;{clicks}</p>          {/* displaying the heart and the number of clicks on the page. */}
       </Card>
     );
         
